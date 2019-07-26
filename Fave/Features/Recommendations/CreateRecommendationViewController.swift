d679d41e47aa565047f0cf13454d26a230efecf6
Patrick Reynolds <patrickscottreynolds@gmail.com>
2019-07-25 17:58:56 -0700

    Bump Cocoapods version and update placeholder copy

--- Fave/Features/Recommendations/CreateRecommendationViewController.swift
+++ Fave/Features/Recommendations/CreateRecommendationViewController.swift
@@ -191,7 +191,7 @@ class CreateRecommendationViewController: FaveVC {
         noteTextView.layer.cornerRadius = 6
         noteTextView.textContainerInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
 
-        noteTextViewPlaceholder = Label(text: "Leave a note...", font: FaveFont(style: .h5, weight: .regular), textColor: FaveColors.Black50, textAlignment: .left, numberOfLines: 1)
+        noteTextViewPlaceholder = Label(text: "(Optional) Leave a note...", font: FaveFont(style: .h5, weight: .regular), textColor: FaveColors.Black50, textAlignment: .left, numberOfLines: 1)
         noteTextViewPlaceholder.translatesAutoresizingMaskIntoConstraints = false
         noteTextViewPlaceholder.isHidden = false
 
